23853eb5b8640e47aa85ffc85ebc634924fa4ee6
agrassi3 <agrassi@udel.edu>
2017-11-08 21:20:54 -0500

    Attempt to deblur with the images from the paper.

--- projectSetUp.m
+++ projectSetUp.m
@@ -9,7 +9,7 @@
 % https://www.mathworks.com/help/images/examples/deblurring-images-using-the-blind-deconvolution-algorithm.html
 %%%
 
-image = imread('distantPlanet.jpg');  % change name to fit the desired image file name
+image = imread('PAPER-Original_Image.png');  % change name to fit the desired image file name
 
 % display the original image
 figure();
@@ -28,10 +28,16 @@ figure();
 imshow(blurredImage);
 title('Blurred Image');
 
+% display the paper blurred image
+paper_blurred = imread('PAPER-Blurred_Image.png');
+figure();
+imshow(paper_blurred);
+title('Paper Blurred Image');
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Restore image using PSF of various sizes ...
-undersized = ones(size(psf)-4);
-[j1, p1] = deconvblind(blurredImage,undersized);
+undersized = ones([7 7]);
+[j1, p1] = deconvblind(paper_blurred,undersized);
 
 % display attempt at deblur
 figure();
@@ -39,27 +45,82 @@ imshow(j1);
 title('Deblurring with Undersized PSF');
 
 oversized = padarray(undersized,[4 4], 'replicate', 'both');
-[j2, p2] = deconvblind(blurredImage,oversized);
+[j2, p2] = deconvblind(paper_blurred,oversized);
 
 % display attempt at deblur
 figure();
 imshow(j2);
 title('Deblurring with Oversized PSF');
 
+% Try again using returned outputs
+[j3, p3] = deconvblind(paper_blurred,p1);
+
+figure();
+imshow(j3);
+title('Deblurring Try2');
+
+% Try again using returned outputs
+[j4, p4] = deconvblind(paper_blurred,p2);
+
+figure();
+imshow(j4);
+title('Deblurring Try3');
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Improving using Weights
 % https://www.mathworks.com/help/images/ref/deconvblind.html?s_tid=srchtitle
 
-WT = zeros(size(blurredImage));
-WT(5:end-4,5:end-4) = 1;
-INITPSF = ones(size(psf));
+% WEIGHT = edge(paper_blurred(:,:,1),'sobel',.28);
+% se1 = strel('disk',1);
+% se2 = strel('line',13,45);
+% WEIGHT = ~imdilate(WEIGHT,[se1 se2]);
+% WEIGHT = padarray(WEIGHT(2:end-1,2:end-1),[1 1]);
+% 
+% figure();
+% imshow(WEIGHT);
+% title('Weight Array');
+% 
+% P1 = p2;
+% P1(find(P1 < 0.01))= 0;
+% 
+% [J2 P2] = deconvblind(paper_blurred(:,:,1),P1,50,[],double(WEIGHT));
+% 
+% figure();
+% imshow(J2);
+% title('Newly Deblurred Image');
+% 
 
-figure(); 
-imshow(WT); 
-title('Weight array');
 
-[J , P] = deconvblind(blurredImage,INITPSF,50,[],WT);
+%% New Image - Attempt
 
+image = imread('PAPER-Original_Image.png');  % change name to fit the desired image file name
+
+% display the original image
 figure();
-imshow(J);
+imshow(image);
+title('Original Image');
+
+% display the paper blurred image
+paper_blurred = imread('PAPER-Blurred_Image.png');
+figure();
+imshow(paper_blurred);
+title('Paper Blurred Image');
+
+% Restore image using PSF of various sizes ...
+
+% ATTEMPT 1
+attempt1 = ones([7 7]);
+[j1, p1] = deconvblind(paper_blurred,attempt1);
+
+% display attempt at deblur
+figure();
+imshow(j1);
+title('Deblurring with Guess 1');
+
+% ATTEMPT 2
+[j2, p2] = deconvblind(paper_blurred,p1);
+
+% display attempt at deblur
+figure();
+imshow(j2);
+title('Deblurring with Guess 2');
